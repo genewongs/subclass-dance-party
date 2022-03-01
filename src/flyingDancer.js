28e23ab0e1a19532cfc5fc43cadb9c90410421c3
Gene Wong <gene.h.wong@gmail.com>
2022-03-01 11:17:38 -0800

    implemented some of flyingDancer.js

--- src/flyingDancer.js
+++ src/flyingDancer.js
@@ -1,5 +1,6 @@
 var FlyingDancer = function(top, left, timeBetweenSteps) {
-
+  this.timeBetweenSteps = timeBetweenSteps;
+  Dancer.apply(this, arguments);
 };
 
 //Prototypes
@@ -11,6 +12,8 @@ FlyingDancer.prototype.constructor = FlyingDancer;
 FlyingDancer.prototype.step = function() {
   Dancer.prototype.step.call(this);
 
+  //for when we deal with window
+  var dancerHeight = this.$node.position();
   //changes position
-
+  this.$node.addClass('fly');
 };
